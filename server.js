abc430976cf1b4d40108801c3cca74916c60ecfd
cis101 <cis101@secs.iub.edu.bd>
2019-11-04 09:40:51 +0600

    Class work 8a

---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var fs = require('fs')
+  var server = http.createServer(function(req, res){
+    
+
+    fs.readFile('index.html', function(err,data){
+        if(err){
+                res.statusCode = 400;
+                res.json({'err':'File not found'})
+        }
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(data)
+    })
+    //res.end("Hello World\n");
+  });
+  server.listen(3000, 'localhost', function(){
+    console.log('Server running');
+  });
