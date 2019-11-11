316152c27530a693c7716e12ff7db4855817a63c
cis101 <cis101@secs.iub.edu.bd>
2019-11-11 09:16:06 +0600

    class work 8d

--- server.js
+++ server.js
@@ -5,19 +5,32 @@ var server = http.Server(app);
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:true}))
 
+let articles = []
+
 app.post('/submit',function(request,response){
-  console.log(request.body)
+  articles.push(request.body)
+  console.log(articles)
   response.json({msg:"successfully received"})
+}) 
+
+app.get('/article/:index',function(request,response){
+ if(articles[request.params.index]){
+   //response.json(articles[request.params])
+   response.render('article.ejs',{article:articles[request.params.index]})
+ }
+ else{
+   response.json({msg:"Article not found"})
+ }
 })
 
 app.get('/',function(request,response){
-  response.sendFile(__dirname+'/index.html')
+  response.sendFile(__dirname+'/views/index.html')
 })
 app.get('/second',function(request,response){
-  response.sendFile(__dirname+'/second.html')
+  response.sendFile(__dirname+'/views/second.html')
 })
 app.get('/new',function(request,response){
-  response.sendFile(__dirname+'/form.html')
+  response.sendFile(__dirname+'/views/form.html')
 })
   server.listen(3000, 'localhost', function(){
     console.log('Server running');
