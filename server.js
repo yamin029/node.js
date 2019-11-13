33f92fee0fb6410a69029855acd75072b87d69c7
cis101 <cis101@secs.iub.edu.bd>
2019-11-13 09:21:58 +0600

    9a

--- server.js
+++ server.js
@@ -3,11 +3,30 @@ var express = require('express');
 var app = express();
 var server = http.Server(app);
 var bodyParser = require('body-parser')
+
+var mongo = require('mongodb')
+var db, uri = "mongodb+srv://yaminkhan017:@Yamin287232@cluster0-95fpq.mongodb.net/test?retryWrites=true&w=majority"
+mongo.MongoClient.connect(uri,
+  {useNewUrlParser:true, useUnifiedTopology:true},function(err,client){
+    if(err){
+      console.log('could not connect to MongoDB')
+    }else{
+      db = client.db('node-cw9')
+    }
+  })
+
+var save = function(form_data){
+  db.createCollection('articles',function(err, co){})
+  var collection = db.collection('articles')
+  collection.save(form_data)
+}
+
 app.use(bodyParser.urlencoded({extended:true}))
 
 let articles = []
 
 app.post('/submit',function(request,response){
+  save(request.body)
   articles.push(request.body)
   console.log(articles)
   response.json({msg:"successfully received"})
